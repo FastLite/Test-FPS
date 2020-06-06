5ed5fbf1ffac8a1ab7bb14cf653eef3af4e8559f
FastLite <Volodin_vlad2002@mail.ru>
2020-06-06 00:59:26 -0400

    Change to round bullet

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,15 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class bullet : MonoBehaviour
+public class Enemy : MonoBehaviour
 {
-    
+    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-
+    // Update is called once per frame
     void Update()
     {
         
